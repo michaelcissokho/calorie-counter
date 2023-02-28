6d3a00a4095ceb2d92acf2b57ae3425eb24fbfdf
Michael Cissokho <michael.cissokho@gmail.com>
2023-02-28 16:20:36 -0500

    update port declaration

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 const app = require('./app')
 dotenv.config()
 
-const PORT = process.env.NODE_ENV == 'test' ? 5000 : 5000
+const PORT = +process.env.PORT || 5000
 
 app.listen(PORT, function(){
     console.log(`Started on http://localhost:${PORT}`)
