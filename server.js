9ad5a2b4fb557db03a8c743a8532ae49f553b370
Michael Cissokho <michael.cissokho@gmail.com>
2023-03-01 13:03:46 -0500

    update app.listen statement

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ dotenv.config()
 const PORT = +process.env.PORT || 5000
 
 app.listen(PORT, function(){
-    console.log(process.env.NODE_ENV)
+    console.log('NODE ENVIRONMENT:', process.env.NODE_ENV)
     if (process.env.NODE_ENV == 'production'){
         console.log(`Started on "https://calorific.herokuapp.com" on port ${PORT}`)
     }else{
         console.log(`Started on http://localhost:${PORT}`)
     }
-    
 })
